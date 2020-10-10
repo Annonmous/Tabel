89376229d2ed11f1943ea728e3c430a229646486
Muhammad Huzaifa Khan <35940055+Annonmous@users.noreply.github.com>
2020-10-10 22:38:59 +0500

    tabel

---
+++ code.js
@@ -0,0 +1,15 @@
+let arr =[2 , 3 ,4];
+
+let i,s;
+
+for(i=0;i<3;i++)
+{
+  s=1
+  console.log("TABLE OF",arr[i]);
+ 
+  while(s<11)
+  {
+  console.log(arr[i]," X ",s," = ",arr[i]*s);
+  s++;
+  }
+}
